67b018df5513af3be335662f03d4e375b26575ec
alan <alan001tk@gmail.com>
2023-03-29 16:59:45 -0700

    Fix typo

--- .config/scripts/setup_void.sh
+++ .config/scripts/setup_void.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# WARNING: This script is meant for fresh installs. It will delete
+# certain files.
 # Run this script as sudo
 
 # Root directory here
 linux_config=$(git rev-parse --show-toplevel)
 
 # Setup packages
-xbps-install -Su $(cat ${linux_config}void_linux_pkglist.txt)
+xbps-install -Su $(cat ${linux_config}/void_linux_pkglist.txt)
 # Setup permissions
 printf "permit :wheel\n" > /etc/doas.conf
 
@@ -14,15 +16,17 @@ printf "permit :wheel\n" > /etc/doas.conf
 source setup_dirs.sh
 
 # Make symlinks
-rm -i ${HOME}/.bash_profile
+rm ${HOME}/.bash_profile
 ln -s ${linux_config}/bash_files/.bash_profile ${HOME}
 source ${HOME}/.bash_profile
-rm -i ${HOME}/.bashrc
+
+rm ${HOME}/.bashrc
 ln -s ${linux_config}/bash_files/.bashrc ${HOME}
 source ${HOME}/.bashrc
-rm -rfi ${HOME}/.config
+
+rm -rf ${HOME}/.config
 ln -s ${linux_config}/.config ${HOME}  
-rm -i ${HOME}/.gitconfig
+rm ${HOME}/.gitconfig
 ln -s ${linux_config}/.config/git/.gitconfig ${HOME}
 
 # Setup programs
